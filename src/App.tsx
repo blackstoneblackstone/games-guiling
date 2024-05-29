63af2d21035f5ae30fbba518befdea7f9e122db0
yongdu.lhb <yongdu.lhb@alibaba-inc.com>
2024-05-30 01:12:07 +0800

    修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Row, Col } from 'antd';
-import { getTwoArray } from './constants'
+import { getTwoArray } from './constants';
 import { useState } from 'react';
 
-import './App.css'
+import './App.css';
 
 const data = getTwoArray();
 
@@ -17,33 +17,35 @@ function App() {
       }
     }
     e.target.parentNode.classList.add('is-flipped');
-  }
+  };
 
   return (
     <div className="App">
       <Row justify="center">
         <div className="sum">
-          <div className="title">
-            好运翻不停，只要别归零
-          </div>
-          <div className="score">
-            总分: {s} 分
-          </div>
+          <div className="title">好运翻不停，只要别归零</div>
+          {/* <div className="score">总分: {s} 分</div> */}
         </div>
       </Row>
-      {data.map((item: number[], row: number) => (<Row gutter={10}>
-        {item.map((it: number, col: number) =>
-        <Col key={row+'-'+col} span={4}>
-        <div className="card" onClick={(e) => onClick(e, it)}>
-            <div className="card-face card-face-front">{row+1}-{col+1}</div>
-            <div className="card-face card-face-back">{it === 0 ? '归零' : it}</div>
-        </div>
-        </Col>
-      )}
-      </Row>
+      {data.map((item: number[], row: number) => (
+        <Row gutter={10}>
+          {item.map((it: number, col: number) => (
+            <Col key={row + '-' + col} span={4}>
+              <div className="card" onClick={(e) => onClick(e, it)}>
+                <div className="card-face card-face-front">
+                  {row + 1}-{col + 1}
+                </div>
+                <div className="card-face card-face-back">
+                  {it === 0 ? '归零' : it}
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
       ))}
+      <p style={{ fontSize: 20 }}>游戏开发者【万晴】，祝大家玩得开心！</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/constants.ts
+++ src/constants.ts
@@ -1,35 +1,31 @@
-export const data1 = [
-]
+export const data1 = [];
 
 export const data = [
-  1,1,1,1,1,1,
-  2,2,2,2,2,2,
-  3,3,3,3,3,3,
-  4,4,4,4,4,4,
-  5,5,5,5,5,5,
-]
+  1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5,
+  5, 5, 5, 5,
+].map((item) => item * 5);
 
 const getRandomArray = (arr: number[]) => {
   // 从数组中挑选出一个元素
   for (var i = arr.length - 1; i > 0; i--) {
-  // 从剩余元素中随机选择另一个元素
-  var j = Math.floor(Math.random() * (i + 1));
-  // 交换位置
-  var temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
+    // 从剩余元素中随机选择另一个元素
+    var j = Math.floor(Math.random() * (i + 1));
+    // 交换位置
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
   return arr;
-}
+};
 
 export const getTwoArray = () => {
   const d = getRandomArray(data);
   const curData = [];
   for (let i = 0; i < 6; i++) {
-    const c = d.slice(i * 5, (i * 5 + 5));
-    c.splice(Math.floor(Math.random() * 5 * 10), 0, 0);
+    const c = d.slice(i * 5, i * 5 + 5);
+    c.splice(Math.floor(Math.random() * 5), 0, 0);
     curData.push(c);
   }
-  console.log('curData',curData);
+  console.log('curData', curData);
   return curData;
-}
+};
